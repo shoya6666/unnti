1ed646a8c2d40d4726627d2a7a7d68b798145d65
翔也 <@DESKTOP-2QM3MR2>
2025-08-10 15:30:12 +0900

    0810

--- unnti/main.cpp
+++ unnti/main.cpp
@@ -9,9 +9,14 @@ const char kWindowTitle[] = "GC1C_08_タナカ_ショウヤ_タイトル";
 const int kWindowWidth = 1280;
 const int kWindowHeight = 720;
 
+uint32_t color_ = WHITE;
 
 struct Vector3 {
 	float x, y, z;
+	// 減算
+	Vector3 operator-(const Vector3& other) const {
+		return { x - other.x, y - other.y, z - other.z };
+	}
 };
 struct Matrix4x4
 {
@@ -512,7 +517,7 @@ Vector3 Project(const Vector3& v1, const Vector3& v2) {
 	return { scale * v2.x, scale * v2.y, scale * v2.z };           // 射影ベクトルを返す
 }
 
-Vector3 ClosestPoin(const Vector3& point, const Segment& segment) {
+Vector3 ClosestPoint(const Vector3& point, const Segment& segment) {
 	// 始点
 	const Vector3& a = segment.origin;
 
@@ -549,12 +554,27 @@ Vector3 ClosestPoin(const Vector3& point, const Segment& segment) {
 	return closest;
 }
 
+bool IsCollision(const Sphere& s1, const Sphere& s2) {
+
+	float distance = Length(s2.center - s1.center);
+
+	if (distance <= s1.radius + s2.radius) {
+
+		color_ = RED;
+
+		return true;
+	}
+	else {
+		color_ = WHITE;
+		return false;
+	}
+};
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
-	Novice::Initialize(kWindowTitle, 1280, 720);
+	Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
 
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
@@ -563,17 +583,21 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Vector3 cameraRotate{ 0.26f,0.0f,0.0f };
 	Vector3 cameraTranslate{ 0.0f,1.9f,-6.49f };
 
-	Sphere sphere = { 0.0f,0.0f,1.0f,1.0f };
+	Sphere sphere1 = { 0.0f,0.0f,1.0f,1.0f };
+	Sphere sphere2 = { 2.0f,0.0f,1.0f,0.5f };
 
-	Segment segment{ {-2.0f,-1.0f,0.0f},3.0f,2.0f,2.0f };
+	Segment segment{ {-2.0f,-1.0f,0.0f},{3.0f,2.0f,2.0f} };
+
 	Vector3 point{ -1.5f,0.6f,0.6f };
-	
+
 	Vector3 project = Project(Subtract(point, segment.origin), segment.diff);
 
-	Vector3 closestPoint = ClosestPoin(point, segment);
+	Vector3 closestPoint = ClosestPoint(point, segment);
 
 	Sphere pointSphere{ point,0.01f };
+
 	Sphere closestPointSphere{ closestPoint,0.01f };
+
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -589,22 +613,26 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 
 
+
+
 		Matrix4x4 cameraMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, cameraRotate, cameraTranslate);
 		Matrix4x4 viewMatrix = Inverse(cameraMatrix);
 		Matrix4x4 projectionMatrix = MakePerspectiveForMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
 		Matrix4x4 viewProjectionMatrix = Multiply(viewMatrix, projectionMatrix);
 		Matrix4x4 viewportMatrix = MakeViewportMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
-		Vector3 start = Transform(Transform(segment.origin, viewProjectionMatrix), viewportMatrix);
-		Vector3 end = Transform(Transform(add(segment.origin, segment.diff), viewProjectionMatrix), viewportMatrix);
-		
+
+		IsCollision(sphere1, sphere2);
+
 		ImGui::Begin("Window");
 		ImGui::DragFloat3("CameraTranslate", &cameraTranslate.x, 0.01f);
 		ImGui::DragFloat3("CameraRotate", &cameraRotate.x, 0.01f);
-		ImGui::DragFloat3("SphereCenter", &sphere.center.x, 0.01f);
-		ImGui::DragFloat("SphereRadius", &sphere.radius, 0.01f);
+		ImGui::DragFloat3("SphereCenter", &sphere1.center.x, 0.01f);
+		ImGui::DragFloat("SphereRadius", &sphere1.radius, 0.01f);
 		ImGui::InputFloat3("Project", &project.x, "%.3f", ImGuiInputTextFlags_ReadOnly);
 		ImGui::End();
-		
+
+
+		///
 		/// ↑更新処理ここまで
 		///
 
@@ -613,10 +641,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 
 		DrawGrid(viewProjectionMatrix, viewportMatrix);
-		DrawSphere(pointSphere, viewProjectionMatrix, viewportMatrix, RED);
-		DrawSphere(closestPointSphere, viewProjectionMatrix, viewportMatrix, BLACK);
 
-		Novice::DrawLine(int(start.x), int(start.y), int(end.x), int(end.y), WHITE);
+		DrawSphere(sphere1, viewProjectionMatrix, viewportMatrix, color_);
+
+		DrawSphere(sphere2, viewProjectionMatrix, viewportMatrix, WHITE);
+
+
 
 		///
 		/// ↑描画処理ここまで
